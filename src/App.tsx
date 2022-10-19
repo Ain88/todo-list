52a719d26b55f3661c576d166c12f443ad10245c
ain88 <ahyungss@hotmail.com>
2022-10-18 22:52:35 -0700

    Added recoli :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { atom, selector, useRecoilValue } from 'recoil';
+
+const todoIdStatte = atom({
+  key: 'todoIdState',
+  default: 1
+});
 
 function App() {
   return (
